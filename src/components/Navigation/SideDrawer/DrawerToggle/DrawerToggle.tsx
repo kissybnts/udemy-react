36ab72f8b3fae5aeff3f6f6a2770f58325ffd4f6
Masataka Kishida <fstt.fwmym.f15@gmail.com>
2017-12-23 17:32:03 +0900

    Lecture 135, Add Menu icon

--- src/components/Navigation/SideDrawer/DrawerToggle/DrawerToggle.tsx
+++ src/components/Navigation/SideDrawer/DrawerToggle/DrawerToggle.tsx
@@ -1,11 +1,16 @@
 import * as React from 'react';
+import * as cssClasses from './DrawerToggle.css';
 
 interface Props {
   clicked: () => void;
 }
 
 const drawerToggle: React.SFC<Props> = props => (
-  <div onClick={props.clicked}>MENU</div>
+  <div className={cssClasses.DrawerToggle} onClick={props.clicked}>
+    <div/>
+    <div/>
+    <div/>
+  </div>
 );
 
 export default drawerToggle;
