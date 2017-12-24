7f294498dafe37864c8417f253fa5fb78a25b866
Masataka Kishida <fstt.fwmym.f15@gmail.com>
2017-12-24 23:09:28 +0900

    Lecture 204, Set up Routing and Routes

--- src/App.tsx
+++ src/App.tsx
@@ -2,14 +2,17 @@ import * as React from 'react';
 import Layout from './containers/Layout/Layout';
 import BurgerBuilder from './containers/BurgerBuilder/BurgerBuilder';
 import Checkout from './containers/Checkout/Checkout';
+import { Route, Switch } from 'react-router';
 
 class App extends React.Component {
   render() {
     return (
       <div>
         <Layout>
-          <BurgerBuilder/>
-          <Checkout/>
+          <Switch>
+            <Route path={'/checkout'} component={Checkout}/>
+            <Route path={'/'} exact component={BurgerBuilder}/>
+          </Switch>
         </Layout>
       </div>
     );
--- src/index.tsx
+++ src/index.tsx
@@ -3,9 +3,15 @@ import * as ReactDOM from 'react-dom';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 import './index.css';
+import { BrowserRouter } from 'react-router-dom';
+
+const app = (
+  <BrowserRouter>
+    <App/>
+  </BrowserRouter>);
 
 ReactDOM.render(
-  <App/>,
+  app,
   document.getElementById('root') as HTMLElement
 );
 registerServiceWorker();
