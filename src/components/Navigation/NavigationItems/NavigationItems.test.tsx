9f4d8e241b5ae97e56d35c1032b08a2c49357a12
Masataka Kishida <fstt.fwmym.f15@gmail.com>
2018-01-13 12:36:10 +0600

    Lecture 339, Wrighting authenticated pattern test for NavigationItems using another wrappter method

--- src/components/Navigation/NavigationItems/NavigationItems.test.tsx
+++ src/components/Navigation/NavigationItems/NavigationItems.test.tsx
@@ -23,4 +23,10 @@ describe('<NavigationItems />', () => {
     wrapper.setProps({ isAuthenticated: true });
     expect(wrapper.find(NavigationItem)).toHaveLength(3);
   });
+
+  it('should render logout <NavigationItem/> element if authenticated', () => {
+    wrapper.setProps({ isAuthenticated: true });
+    expect(wrapper.contains(<NavigationItem link={'/logout'} exact={false}>Logout</NavigationItem>))
+      .toEqual(true);
+  });
 });
